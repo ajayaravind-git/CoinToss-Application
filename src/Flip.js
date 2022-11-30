9126a24a7f2eb598652e614c20d699079bc0b198
Ajay Aravind <ajayaravind.me@gmail.com>
2022-11-30 23:44:15 +0530

    added basic functions including Flip function

--- src/Flip.js
+++ src/Flip.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
+import Coin from './Coin'
+import { choice } from './helpers'
 class Flip extends Component {
+    static defaultProps = {
+        coins: [{ side: 'head', imgSrc: 'https://tinyurl.com/react-coin-heads-jpg' },
+        { side: 'tails', imgSrc: 'https://tinyurl.com/react-coin-tails-jpg' }]
+    }
     constructor(props) {
         super(props);
         this.state = {
@@ -10,10 +16,19 @@ class Flip extends Component {
         }
 
     }
+    flipCoin() {
+        const newCoin = choice(this.props.coins);
+    }
+    handleClick = (e) => {
+        this.flipCoin();
+    }
     render() {
-        return (<div>
+        return (<div className='coinContainer'>
             <h1>Lets Flip a coin!</h1>
-            <button>Flip</button>
+            <button onClick={this.handleClick}>Flip</button>
+            <p>
+                Out of {this.state.nFlips} flips, {this.state.nHeads} were Head and {this.state.nTails} were Tail.
+            </p>
         </div>)
     }
 }
