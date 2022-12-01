2c99a230b8c2f07640e8042e2cb450856b37fb90
Ajay Aravind <ajayaravind.me@gmail.com>
2022-12-01 13:29:29 +0530

    coin displayed and styled

--- src/Coin.js
+++ src/Coin.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
+import './Coin.css'
 class Coin extends Component {
 
     render() {
-
+        return <img className='coinImage' src={this.props.info.imgSrc} alt={this.props.info.side} />
     }
 }
 
--- src/Flip.js
+++ src/Flip.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import Coin from './Coin'
+import "./Flip.css"
 import { choice } from './helpers'
 class Flip extends Component {
     static defaultProps = {
-        coins: [{ side: 'heads', imgSrc: 'https://tinyurl.com/react-coin-heads-jpg' },
-        { side: 'tails', imgSrc: 'https://tinyurl.com/react-coin-tails-jpg' }]
+        coins: [{ side: 'heads', imgSrc: 'https://qph.cf2.quoracdn.net/main-qimg-9c81a54813716fccd8e3608ab2f51dcf-lq' },
+        { side: 'tails', imgSrc: 'https://qph.cf2.quoracdn.net/main-qimg-148ae81e6fe0500e130fb547026a9b26-lq' }]
     }
     constructor(props) {
         super(props);
@@ -20,14 +21,10 @@ class Flip extends Component {
         const newCoin = choice(this.props.coins);
         this.setState(st => {
             let newState = {
-                ...st,
                 currCoin: newCoin,
-                nFlips: st.nFlips + 1
-            }
-            if (newCoin.side === 'heads') {
-                newState.nHeads += 1;
-            } else {
-                newState.nTails += 1;
+                nFlips: st.nFlips + 1,
+                nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+                nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0)
             }
 
             return newState
@@ -39,8 +36,10 @@ class Flip extends Component {
     render() {
         return (<div className='coinContainer'>
             <h1>Lets Flip a coin!</h1>
-            <button onClick={this.handleClick}>Flip</button>
-            <p>
+
+            {(this.state.currCoin !== null) && (<Coin info={this.state.currCoin} />)}<br></br>
+            <button className='clickbutton' onClick={this.handleClick}>Flip</button>
+            <p className='coinDetails'>
                 Out of {this.state.nFlips} flips, {this.state.nHeads} were Head and {this.state.nTails} were Tail.
             </p>
         </div>)
